4edb92ad305d56f63f8fd37ddc2da490865a2973
raziq <raziqsufian2002@gmail.com>
2026-05-01 11:10:48 +0800

    adjust middleware

--- server/middlewares/exceptions_handlers.py
+++ server/middlewares/exceptions_handlers.py
@@ -1,12 +1,11 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from logger import Logger
+from logger import logger
 
 async def catch_exception_middleware(request:Request, call_next):
     try:
         response = await call_next(request)
         return response
     except Exception as e:
-        Logger.exception(f"UNHANDLED EXCEPTIONS")
+        logger.exception(f"UNHANDLED EXCEPTIONS")
         return JSONResponse(status_code=500, content={"error": str(e)})
-    
